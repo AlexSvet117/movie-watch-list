775d9a6d72d21b2309155640c30715eac8d76539
AlexSvet117 <alex.svet117@gmail.com>
2025-01-25 22:16:06 +0000

    WIP

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 import { FaTrashCan } from "react-icons/fa6";
+import MovieList from "./MovieList";
 
-function MovieCard() {
+function MovieCard(movie) {
   return (
-    <div className="col-6 m-5">
+    <div className="col-6">
       <div className="card bg-bone-white text-dark">
-        <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
-          99+
-          <span class="visually-hidden">unread messages</span>
+        <span class="position-absolute top-0 start-0 translate-middle badge bg-success custom-badge-position">
+          {/* <span class="visually-hidden">unread messages</span> */}
+          {movie.rate}
+          
         </span>
         <div className="card-body d-flex justify-content-between">
-          <h3 className="card-title m-0">Card</h3>
+          <h3 className="card-title m-0">{movie.title}</h3>
           <div className="text-danger">
             <FaTrashCan />
           </div>
---
+++ src/components/MovieList.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react"
+import MovieCard from "./MovieCard"
+
+function MovieList() {
+
+    const [movies, setMovies] = useState([
+        {
+            id: 1001,
+            title: "Inception",
+            rate: 8.7
+        }, 
+        {
+            id: 1002,
+            title: "Titanic",
+            rate: 9.0
+        },
+        {
+            id: 1003,
+            title: "Harry Potter",
+            rate: 9.0
+        }
+        ])
+
+  return (
+    <div className="d-flex flex-column gap-3 align-items-center mt-5">
+        {movies.map((movie)=>(
+            <MovieCard key={movie.id} movie={movie}/>
+        ))}
+      
+    </div>
+  )
+}
+
+export default MovieList
