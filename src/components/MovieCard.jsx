90909d7cb3f4a267f7a6379464d4eecabe7b9233
AlexSvet117 <alex.svet117@gmail.com>
2025-01-25 22:19:45 +0000

    WIP

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import { FaTrashCan } from "react-icons/fa6";
-import MovieList from "./MovieList";
 
 function MovieCard(movie) {
   return (
     <div className="col-6">
       <div className="card bg-bone-white text-dark">
-        <span class="position-absolute top-0 start-0 translate-middle badge bg-success custom-badge-position">
-          {/* <span class="visually-hidden">unread messages</span> */}
+        <span className="position-absolute translate-middle badge bg-success custom-badge-position">
+          <span className="visually-hidden">unread messages</span>
           {movie.rate}
           
         </span>
